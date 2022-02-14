1dc2b89aa8f74de3868f00a0d735867f59e7e23f
Sebastian Uyaguari <65028259+SUyaguari@users.noreply.github.com>
2022-02-14 17:38:50 -0500

    Funcionamiento ventas
    con servicios

---
+++ src/app/domain/ventas.ts
@@ -0,0 +1,9 @@
+export class ventas{
+
+    codigo: number =0 ;
+    codigoProducto : string ='';
+    total: number = 0;
+    cantidad: number =0;
+    vendedor: string ='';
+    comprador : string ='';
+}
